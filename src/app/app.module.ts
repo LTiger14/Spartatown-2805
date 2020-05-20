eb71677de5442aaac250678f810b3279a7a31905
Pierrestiger, Louis <louis.pierrestiger@sap.com>
2020-05-20 14:51:39 -0400

    schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {
+  BrowserModule,
+  BrowserTransferStateModule,
+} from '@angular/platform-browser';
+import { translationChunksConfig, translations } from '@spartacus/assets';
+import { B2cStorefrontModule } from '@spartacus/storefront';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
+    B2cStorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl:
+            'https://api.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com',
+          prefix: '/rest/v2/',
+        },
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en',
+      },
+      features: {
+        level: '1.5',
+        anonymousConsents: true,
+      },
+    }),
+    BrowserTransferStateModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
